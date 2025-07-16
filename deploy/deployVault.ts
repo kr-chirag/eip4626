3891b8e5893ae3b66303f159c0502f4be9bd4191
kr-chirag <chirag.p@knackroot.com>
2025-07-16 10:23:58 +0530

    removed deployments

--- deploy/deployVault.ts
+++ deploy/deployVault.ts
@@ -4,9 +4,12 @@ import {ethers} from 'hardhat';
 import AddressBook from '../utils/AddressBook';
 
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+
+  const DECIMALS_OFFSET = 0;
+
   console.log("deploying Vault ...");
   const VaultFactory = await ethers.getContractFactory("Vault");
-  const Vault = await VaultFactory.deploy();
+  const Vault = await VaultFactory.deploy(DECIMALS_OFFSET);
   await Vault.waitForDeployment();
   AddressBook.set("Vault", await Vault.getAddress())
   console.log("🚀 ~ Vault address:", AddressBook.get("Vault"))
